9d658cced7b4b41b12a46a5ba11d61894c0dec29
lucas-jorge <lucasajorge@gmail.com>
2025-04-29 13:44:21 -0300

    Removed unused Account class

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
-from escola.models import Estudante,Curso
+from escola.models import Estudante, Curso
+
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
-        fields = ['id', 'nome', 'email', 'CPF', 'data_nascimento', 'numero_celular']
-        
+        fields = [
+            'id', 'nome', 'email', 'CPF', 'data_nascimento', 'numero_celular'
+        ]
+
+
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
-        
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
